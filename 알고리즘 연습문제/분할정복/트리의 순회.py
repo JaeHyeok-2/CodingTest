a27afd8e57164e9d2b3fd3d4e8019dd26991517d
JaeHyeok-2 <dlwogurgur@naver.com>
2022-01-24 22:11:43 +0900

    /

--- 알고리즘 연습문제/분할정복/트리의 순회.py
+++
@@ -1,9 +0,0 @@
-class BinaryTree(object) :
-    def __init__(self):
-        self.root = None
-
-class Node(object) :
-    def __init__(self,data):
-        self.data = data
-        self.left =self.right = None
-
